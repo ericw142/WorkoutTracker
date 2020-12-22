4f9002d4286352278f8746c9d952326805601b8d
ericw142 <66480663+ericw142@users.noreply.github.com>
2020-12-22 14:21:28 -0500

    fixed post and put

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -4,26 +4,41 @@ const db = require("../models");
 module.exports = function(app) {
     app.get("/api/workouts", (req, res) => {
         db.Workout.find({}).then(result => {
-            console.log(result);
-            res.json({result});
+            res.json(result);
+        })
+        .catch(err => {
+            console.log(err);
         })
     })
 
     app.post("/api/workouts", (req, res) => {
-        console.log("Post: "+req.body);
-
-        db.Workout.create(req.body);
+        db.Workout.create(req.body)
+        .then(result => {
+            res.json(result);
+        })
+        .catch(err => {
+            console.log(err);
+        })
     })
 
     app.get("/api/workouts/:range", (req, res) => {
         db.Workout.find({}).then((result) => {
             res.json(result);
         })
+        .catch(err => {
+            console.log(err);
+        })
     })
 
     app.put("/api/workouts/:id", (req, res) => {
-        db.Workout.findOne({ id: req.params.id }).then((result) => {
+        db.Workout.findByIdAndUpdate(req.params.id, 
+        { $push: { "exercises": req.body } }, { new: true }
+        )
+        .then((result) => {
             res.json(result);
+        })
+        .catch(err => {
+            console.log(err);
         })
     })
 }
